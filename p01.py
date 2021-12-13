d3716971ef0c9220f5025113b19407196137089f
Alister McKinley <alpatine@gmail.com>
2021-12-13 16:29:44 +1100

    solve problem 1

---
+++ p01.py
@@ -0,0 +1,16 @@
+def p01():
+    with open("p01_data.txt") as data_file:
+        data = data_file.read().splitlines()
+    
+    increase_count = 0
+    previous_value = 1e10
+    for value_str in data:
+        value = int(value_str)
+        if value > previous_value:
+            increase_count += 1
+        previous_value = value
+    
+    return increase_count
+
+if __name__ == '__main__':
+    print(p01())
